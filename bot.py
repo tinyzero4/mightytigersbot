61ae49390e54670158178c1ca12a7ba9251d9da0
dzmitry.zenin <dzmitry.zenin@pegasus.ae>
2018-04-05 16:49:52 +0300

    refactor

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 import jsonpickle
 from jinja2 import Environment
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING, ASCENDING
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
 from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
@@ -46,9 +46,20 @@ class Player:
 
 
 class Match:
-    def __init__(self, date):
+    def __init__(self, date, team_id):
+        self._id = None
         self.date = date
+        self.team_id = team_id
         self.squad = {}
+        self.completed = False
+
+    @property
+    def match_id(self):
+        return self._id
+
+    def complete(self):
+        self.completed = True
+        return self
 
     def confirm(self, name, username, value):
         if username not in self.squad:
@@ -100,24 +111,22 @@ class Schedule:
 class Team:
     def __init__(self, team_id, schedule, name='MightyTigers'):
         self._id = team_id
-        self.team_id = team_id
         self.name = name
-        self.match = None
         self.schedule = schedule
 
-    def next_match(self):
+    @property
+    def team_id(self):
+        return self._id
+
+    def next_match(self, latest_match):
         next_match_date = self.schedule.next_match_date()
 
-        if not self.match or self.match.date < next_match_date:
-            self.match = Match(next_match_date)
-            return self.match, True
-        return self.match, False
-
-    def __str__(self):
-        return f"Who we are? The `{self.name}` !"
+        if not latest_match or latest_match.date < next_match_date:
+            return Match(next_match_date, self.team_id), latest_match
+        return latest_match, None
 
     def __repr__(self):
-        return f"{self.name}|Current match:{self.match}"
+        return f"name:{self.name}|teamId:{self.team_id}|schedule:{self.schedule}"
 
 
 class GameManager:
@@ -128,7 +137,7 @@ class GameManager:
         tg.dispatcher.add_handler(CallbackQueryHandler(self.on_confirmation))
         tg.dispatcher.add_error_handler(GameManager.on_error)
 
-        self.view = MatchConfirmationView()
+        self.view = ViewHandler()
         self._db = db
         self._tg = tg
 
@@ -150,34 +159,46 @@ class GameManager:
         if team is None:
             team = self.new_team(bot, update)
 
-        (match, is_new) = team.next_match()
-        if is_new:
-            self.update_team(team)
-            self.send_match_stats(bot, team.team_id, match)
+        (next_match, last_match) = team.next_match(self.find_team_latest_match(team.team_id))
+        if last_match:
+            self.save_match(next_match)
+            self.save_match(last_match.complete())
+            ViewHandler.send_match_stats(bot, team.team_id, next_match)
 
     def on_confirmation(self, bot, update):
         if self.__is_unprocessed_update(update) and update.callback_query and update.callback_query.message:
             message = update.callback_query.message
-            team = self.find_team(message.chat_id)
-            if team is not None and team.match is not None:
-                player_profile = update.callback_query.from_user
-                team.match.confirm(player_profile.full_name, player_profile.username, update.callback_query.data)
 
+            team_id = message.chat_id
+            (match_id, confirmation) = ViewHandler.parse_callback_data(update.callback_query.data)
+
+            match = self.find_match(team_id, match_id)
+            if match is not None:
+                player_profile = update.callback_query.from_user
+                match.confirm(player_profile.full_name, player_profile.username, confirmation)
+
+                self.save_match(match)
                 self.__store_update(update.update_id, message.date)
-                self.update_team(team)
-                self.send_match_stats(bot, message.chat_id, team.match, message.message_id)
+
+                ViewHandler.send_match_stats(bot, message.chat_id, match, message.message_id)
         else:
             logger.warning(f"Unknown response type: {update}")
 
-    def find_team(self, team_id):
-        team_def = self._db.teams.find_one({'_id': team_id})
-        return jsonpickle.decode(json.dumps(team_def)) if team_def is not None else None
-
     def save_team(self, team):
-        self._db.teams.insert_one(json.loads(jsonpickle.encode(team)))
+        self._db.teams.insert_one(self.__encode(team))
 
-    def update_team(self, team):
-        self._db.teams.replace_one({'_id': team.team_id}, json.loads(jsonpickle.encode(team)))
+    def find_team(self, team_id):
+        return self.__decode(self._db.teams.find_one({'_id': team_id}))
+
+    def find_team_latest_match(self, team_id):
+        return self.__decode(self._db.matches.find_one({'team_id': team_id})).sort([{'date', DESCENDING}])
+
+    def find_match(self, team_id, match_id):
+        return self.__decode(self._db.matches.find_one({'match_id': match_id, 'team_id': team_id}))
+
+    # TODO: $set by each player confirmation
+    def save_match(self, match):
+        self._db.matches.update_one(filter={'_id': match.team_id}, update=self.__encode(match), upsert=True)
 
     def __is_unprocessed_update(self, update):
         return self._db.confirmations.find_one({'_id': update.update_id}) is None
@@ -186,24 +207,19 @@ class GameManager:
         self._db.confirmations.insert_one({'_id': update_id, 'date': date})
 
     @staticmethod
-    def send_match_stats(bot, chat_id, match, message_id=None):
-        keyboard = [
-            MatchConfirmationView.pack_buttons(CONFIRMATIONS),
-            MatchConfirmationView.pack_buttons(WITH_ME_CONFIRMATIONS)
-        ]
-        if not message_id:
-            return bot.send_message(chat_id, MatchConfirmationView.build_match_stats_view(match), caption="caption",
-                                    parse_mode='html', reply_markup=InlineKeyboardMarkup(keyboard), timeout=5000)
-        else:
-            return bot.edit_message_text(MatchConfirmationView.build_match_stats_view(match), chat_id, message_id,
-                                         parse_mode='html', reply_markup=InlineKeyboardMarkup(keyboard), timeout=5000)
+    def __decode(data):
+        return jsonpickle.decode(json.dumps(data)) if data is not None else None
+
+    @staticmethod
+    def __encode(data):
+        return json.loads(jsonpickle.encode(data))
 
     @staticmethod
     def on_error(bot, update, error):
         logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-class MatchConfirmationView:
+class ViewHandler:
     captions = {CONFIRMATIONS[0]: "[PLAY]", CONFIRMATIONS[1]: "[REJECT]"}
 
     match_stats_view = """
@@ -216,15 +232,40 @@ class MatchConfirmationView:
     _stats_template = Environment().from_string(match_stats_view)
 
     @staticmethod
-    def build_match_stats_view(match):
-        return MatchConfirmationView._stats_template.render(date=match.date, stats=match.stats(),
-                                                            confirmations=CONFIRMATIONS,
-                                                            button_caption=MatchConfirmationView.captions)
+    def send_match_stats(bot, chat_id, match, message_id=None):
+        keyboard = [
+            ViewHandler.pack_buttons(CONFIRMATIONS, [match.match_id]),
+            ViewHandler.pack_buttons(WITH_ME_CONFIRMATIONS, [match.match_id])
+        ]
+        if not message_id:
+            return bot.send_message(chat_id=chat_id, message=ViewHandler.build_match_stats_view(match),
+                                    caption="caption", parse_mode='html', reply_markup=InlineKeyboardMarkup(keyboard),
+                                    timeout=5000)
+        else:
+            return bot.edit_message_text(chat_id=chat_id, text=ViewHandler.build_match_stats_view(match),
+                                         message_id=message_id, parse_mode='html',
+                                         reply_markup=InlineKeyboardMarkup(keyboard), timeout=5000)
 
     @staticmethod
-    def pack_buttons(types):
-        return list(map(lambda t: InlineKeyboardButton(f"{t}{MatchConfirmationView.captions.get(t, '')}",
-                                                       callback_data=f"{t}"), types))
+    def build_match_stats_view(match):
+        return ViewHandler._stats_template.render(date=match.date, stats=match.stats(),
+                                                  confirmations=CONFIRMATIONS,
+                                                  button_caption=ViewHandler.captions)
+
+    @staticmethod
+    def pack_buttons(types, data):
+        return list(
+            map(lambda t: InlineKeyboardButton(f"{t}{ViewHandler.captions.get(t, '')}",
+                                               callback_data=ViewHandler.pack_callback_data(data + [t])), types)
+        )
+
+    @staticmethod
+    def parse_callback_data(data):
+        return data.split(";")
+
+    @staticmethod
+    def pack_callback_data(data):
+        return ";".join(data)
 
 
 def main():
@@ -236,6 +277,7 @@ def main():
         raise ValueError('Mongo URI is not specified')
 
     client.tigers.confirmations.create_index("date", expireAfterSeconds=2 * 24 * 3600)
+    client.tigers.matches.create_index([("team_id", ASCENDING), ("match_id", ASCENDING)])
 
     GameManager(Updater(token), client.tigers).start()
 
