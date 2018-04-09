82438d44525290807ee64756a2b33760038c8ee6
dmitry <dzmitry.zenin@gmail.com>
2018-04-09 06:50:27 +0300

    set webhook

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -7,18 +7,17 @@ variable "aws_config" {
   }
 }
 
-variable "aws_availability_zones" {
-  type = "list"
-  default = [
-    "us-east-1a"
-  ]
+variable "aws_availability_zone" {
+  type = "string"
+  default = "us-east-1a"
 }
 
 variable "aws_security_groups" {
   type = "list"
   # Open ports 80
   default = [
-    "sg-df53bc96"
+    "tigersbot"
+    #  corresponds to sg-df53bc96
   ]
 }
 
@@ -32,11 +31,17 @@ variable "aws_launch_config" {
   type = "map"
   default = {
     # Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type - ami-25615740
-    instance_ami = "ami-428aa838"
+    instance_ami = "ami-1853ac65"
     instance_type = "t2.micro"
     root_size = 8
     key_name = "tigersbot"
   }
 }
 
-variable "tg_bot_token" {}
+variable "tg_bot_token" {
+}
+
+variable "tg_mongo_uri" {
+  type = "string"
+  default = "mongodb://127.0.0.1/tigers"
+}
