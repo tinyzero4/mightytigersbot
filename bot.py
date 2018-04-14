5f38cd391b28fbd13c1bb787f058d494c9921748
dmitry <dzmitry.zenin@gmail.com>
2018-04-14 07:35:51 +0300

    asd

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ class ViewHandler:
     captions = {CONFIRMATIONS[0]: "[PLAY]", CONFIRMATIONS[1]: "[SLEEP]", CONFIRMATIONS[2]: "[?]"}
 
     match_stats_view = """
-|<b>{{date}}</b>|Players: <b><i>{{stats['total']['all']}}</i></b> |
+|<b>{{date}}</b>|Players: <b>{{stats['total']['all']}}</b> |
 {% for c in confirmations %}
 <b>{{c}}[{{stats[c]|length}}]</b>:
 {% for t in stats[c] %}  <i>{{loop.index}}.{{t.name}} {% if t.with_me>0 %}(+{{t.with_me}})\n{% endif %}</i>{% endfor %}
