466a3970f6dff08e6af77960fb3a66ed6652e802
dmitry <dzmitry.zenin@gmail.com>
2018-04-20 13:09:26 +0300

    p1

--- config.js
+++
@@ -1,6 +0,0 @@
-const { MongoClient, ObjectID } = require('mongodb')
-
-export const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/tigers'
-export const db = 'tigers'
-export const teamsCollection = 'teams'
-export const matchesCollection = 'matches'
