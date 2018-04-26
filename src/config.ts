9deb82cc0a1d04be3439accf8d3be7b024d994ce
breedish <breedish@gmail.com>
2018-04-26 06:49:34 +0300

    asd

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN || "588400254:AAHRYluiI7th2eIK4VdskgP_VegSljGqeVk";
 export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/tigers";
 export const DB = "tigers";
-export const DEFAULT_SCHEDULE = [{ day: 1, time: "08:00" }, { day: 4, time: "08:00" }];
+export const DEFAULT_SCHEDULE = [{ day: 1, time: "05:00" }, { day: 4, time: "05:00" }];
 export const CONFIRMATIONS = [
     {v: "⚽", btn: "⚽[PLAY]" }, {v: "💩", btn: "💩[SLEEP]"}, {v: "🤔", btn: "🤔[?]"}
 ];
