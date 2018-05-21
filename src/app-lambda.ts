b2f620ff61aef8c22e8e8ffa75a796a7b8431de0
dzmitry.zenin <dzmitry.zenin@pegasus.ae>
2018-05-21 09:19:41 +0300

    asd

--- src/app-lambda.ts
+++ src/app-lambda.ts
@@ -2,6 +2,7 @@ import "module-alias/register";
 import { bot } from "./bot";
 
 exports.handler = (event, context: any, callback) => {
+  console.info(`${JSON.stringify(event)}`);
   if (context) {
     bot.handleUpdate(JSON.parse(event.body));
   }
