63ca4513c4c968a827d8fb0bcd2b66b669c21ff4
dmitry <dzmitry.zenin@gmail.com>
2018-07-06 22:21:56 +0300

    Update version

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,7 +3,7 @@ export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/tig
 export const DATABASE_NAME = process.env.MONGO_DB || "tigers";
 export const SSL_CERT_PATH = process.env.SSL_CERT_PATH || "./tigers-certificate.pem";
 export const SSL_KEY_PATH = process.env.SSL_KEY_PATH || "./tigers-certificate.key";
-export const VERSION = "1.0.0";
+export const VERSION = "1.0.1";
 
 export const DEFAULT_SCHEDULE = [
   { day: 1, time: "05:00" },
