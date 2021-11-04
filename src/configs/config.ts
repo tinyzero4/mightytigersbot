dcb33dcef792dca66ae6f481e8e4251d47cd7000
dmitry <dzmitry.zenin@gmail.com>
2021-11-05 00:15:47 +0300

    +cert

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,8 +1,8 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN || "613897973:AAErvvp5hsTX61Cz3kshjSADQZx3eeCrjuw";
 export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/tigers";
 export const DATABASE_NAME = process.env.MONGO_DB || "tigers";
-export const SSL_CERT = process.env.SSL_CERT_PATH || "./tigersbot.pem";
-export const SSL_KEY = process.env.SSL_KEY_PATH || "./tigersbot.key";
+export const SSL_CERT = process.env.SSL_CERT_PATH || "/opt/mightytigers/aws.pem";
+export const SSL_KEY = process.env.SSL_KEY_PATH || "/opt/mightytigers/aws.key";
 export const VERSION = "1.0.8";
 
 export const DEFAULT_SCHEDULE = [
